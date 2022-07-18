fe585129933d93b66e8bb9a59871c1a55f31a713
Rhuann-ads <rhuannads@gmail.com>
2022-07-18 15:42:47 -0300

    inclusao do try catch para corrigir entradas com letras

--- src/abduct/Main.java
+++ src/abduct/Main.java
@@ -1,5 +1,7 @@
 package abduct;
 
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Main {
@@ -12,10 +14,10 @@ public class Main {
 		double opcao3 = 250.00;
 		int quantidade = 0;
 		double total = quantidade * opcao1;
-		int codigo;
 
 		
 		menu(sc, opcao1, opcao2, opcao3);
+		
 		
 		sc.close();
 	}
@@ -23,13 +25,18 @@ public class Main {
 	private static void menu(Scanner sc, double opcao1, double opcao2, double opcao3) {
 		int quantidade;
 		double total;
-		int codigo;
+		int codigo = 0;
 		System.out.println("-------MENU--------");
 		System.out.println("1- Oculos esporte");
 		System.out.println("2- Oculos de grau");
 		System.out.println("3- Oculos infantil");
 		
-		codigo = sc.nextInt();
+		try {
+	    	 codigo = sc.nextInt();
+	        } catch (NoSuchElementException | IllegalStateException e) {
+	        	System.out.println("Opção invalida!");
+	        }
+
 		
 		if(codigo == 1) {
 			System.out.println("Quantos oculos esportes? ");
@@ -49,8 +56,8 @@ public class Main {
 			total = quantidade * opcao3;
 			System.out.println("Você adquiriu " + quantidade + 
 					" oculos infantil no valor de: R$" + total + " reais");
-		}else {
-			System.out.println("Opção invalida!");
+		}else{
+			System.out.println("\nOpção invalida!\n");
 			menu(sc, opcao1, opcao2, opcao3);
 		}
 	}
